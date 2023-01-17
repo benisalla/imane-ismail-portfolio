18eb4636e2142c7eaf87d0f46a091cb6cbf27232
ismail ben alla <ismailbenalla52@gmail.com>
2023-01-17 05:53:19 -1100

    i have completed the main animations that rolls in the background

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import { Suspense } from "react";
 import { Canvas } from "react-three-fiber";
-import CarShow from "./Components/BackGround/CarShow";
-import Nature from "./Components/BackGround/Nature";
-
+import Scene from "./Components/Robot/Scene";
 
 function App() {
   return (
     <Suspense fallback={"Loading ..."}>
-      {/* <Canvas shadows>
-        <CarShow />
-      </Canvas> */}
-      <Canvas shadows camera={{ position: [0, 0, 12], fov: 30 }}>
-        <Nature/>
+      <Canvas style={{ width: "100vw", height: "100vh" }} shadows>
+        <Scene />
       </Canvas>
     </Suspense>
   );
