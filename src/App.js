69da386a47631765e0f011640dc80cd0f4ea6c64
ImaneMouatassem <imane12mouatassem@gmail.com>
2023-01-18 16:16:16 +0100

    First Sidebar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Suspense } from "react";
 import { Canvas } from "react-three-fiber";
 import Scene from "./Components/Robot/Scene";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <Suspense fallback={"Loading ..."}>
-      <Canvas style={{ width: "100vw", height: "100vh" }} shadows>
-        <Scene />
-      </Canvas>
-    </Suspense>
+    // <Suspense fallback={"Loading ..."}>
+    //   <Canvas style={{ width: "100vw", height: "100vh" }} shadows>
+    //     <Scene />
+    //     <Sidebar></Sidebar>
+    //   </Canvas>
+    // </Suspense>
+  // <Router>
+  //   <Routes>
+  //     <Route path='/home'  component={Home}/>
+  //   </Routes>
+  // </Router>
+  <div>
+    <Sidebar/>
+  </div>
   );
 }
 
