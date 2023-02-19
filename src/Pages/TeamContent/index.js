51b56d0d67cda5ccd6f15bdfda78f0ba17463edf
Imane Mouatassem <imane12mouatassem@gmail.com>
2023-02-19 01:15:29 +0100

    SOME INFOS ABOUT ME

--- src/Pages/TeamContent/index.js
+++ src/Pages/TeamContent/index.js
@@ -28,12 +28,13 @@ export const TeamContent = () => {
     name: "moutassem imane",
     profession: "computer science engineer",
     description: `
-    I'm a Web Developer, I love to create beautiful and functional websites. 
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, libero? 
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque blanditiis sed aut!
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque blanditiis sed aut!
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque blanditiis sed aut!
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque blanditiis sed aut!
+    Hello, I'm Mouatassem imane, a software engineering student passionate about building innovative and user-friendly software solutions. 
+    As a software engineer, I'm constantly striving to learn and apply new technologies to create robust, efficient,
+     and scalable software solutions. I'm experienced in programming languages such as Java, C++,C# and JavaScript, 
+     and have hands-on experience in building web applications using frameworks like React.js and ASP.NET.
+    In addition to my technical skills, I possess excellent communication, problem-solving, and teamwork skills. 
+    I've worked on various projects both independently and as part of a team, and have experience in all stages of 
+    the software development life cycle.
     `,
     url : "Mouatassem Imane.pdf"
   }
